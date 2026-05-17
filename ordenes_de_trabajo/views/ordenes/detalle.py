b0a39a6758d3652318b76ecfdde2043801ce44c6
Salvador Ortega <salvadorortega505@gmail.com>
2026-05-17 11:41:20 -0500

    refactor ordenes

--- ordenes_de_trabajo/views/ordenes/detalle.py
+++ ordenes_de_trabajo/views/ordenes/detalle.py
@@ -1,37 +1,23 @@
 import uuid
 from decimal import Decimal
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db import transaction
 from django.utils import timezone
 from django.db.models import Q, Count
-from ..api import clasificar_vehiculo_con_ia
-# 🚀 IMPORTAMOS LOS MODELOS DE INVENTARIO NECESARIOS PARA LA "CUARENTENA"
 from inventario.models import CodigoProducto, Categoria
 from servicios.models import ServicioCatalogo
 from ...models import (
-    Sucursal,
-    Cliente,
     OrdenTrabajo,
-    FotoRecepcionVehiculo,
-    OrdenChecklistRecepcion,
     OrdenCroquisDanio,
     OrdenInsumoDetalle,
-    OrdenObjetoAdicional,
     OrdenServicioDetalle,
-    OrdenServicioProcedimientoDetalle, 
-    OrdenSintoma,
-    OrdenTrabajoSolicitado,
-    ExpedienteVehiculo,
-    Cotizacion,                   
-    CotizacionInsumoDetalle,      
-    CotizacionServicioDetalle,
-    CotizacionProcedimientoDetalle
+    OrdenServicioProcedimientoDetalle
 )
 from ..utils import (
-    parse_int, parse_decimal, cargar_json_lista, procesar_imagen_base64,
-    obtener_sucursal_activa, obtener_o_crear_expediente, generar_numero_orden,
+     parse_decimal,
+    obtener_sucursal_activa, 
     puede_operar_orden_desde_sucursal_activa,parse_cantidad
 )
 from django.urls import reverse
