7cb48c3460738afa6ce3e3fa71c3ec4c0c3a65eb
Salvador Ortega <81934578+SalvadorOrt@users.noreply.github.com>
2026-09-06 20:20:27 -0500

    Registrar empresa y firma electronica en admin

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    EmpresaEmisora,
+    FirmaElectronica,
+)
+
+
+@admin.register(EmpresaEmisora)
+class EmpresaEmisoraAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "razon_social",
+        "ruc",
+        "nombre_comercial",
+        "establecimiento",
+        "punto_emision",
+        "activo",
+    )
+
+    search_fields = (
+        "razon_social",
+        "nombre_comercial",
+        "ruc",
+    )
+
+    list_filter = (
+        "activo",
+        "obligado_contabilidad",
+        "agente_retencion",
+    )
+
+
+@admin.register(FirmaElectronica)
+class FirmaElectronicaAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nombre",
+        "empresa",
+        "titular",
+        "ruc",
+        "fecha_inicio_vigencia",
+        "fecha_fin_vigencia",
+        "estado",
+    )
+
+    search_fields = (
+        "nombre",
+        "titular",
+        "ruc",
+        "empresa__razon_social",
+    )
+
+    list_filter = (
+        "estado",
+        "empresa",
+    )
